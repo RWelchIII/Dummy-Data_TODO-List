a641279db7bf9886fbda7b413b06e0a593177579
RWelchIII <Avidcompassion@gmail.com>
2021-12-15 16:56:31 -0600

    best attempt at todos

--- data.js
+++ data.js
@@ -24,9 +24,32 @@
     }
     
     const logTodos = () => {
-
+        console.log(arrayOfTodos)
     }
     
     const populateTodos = () => {
+        var ol = document.getElementById('todo-list');
 
+        for(let i = 0; i < arrayOfTodos.length; i++) {
+
+            var userId = arrayOfTodos[i].userId;
+            var li = document.createElement('li');
+            ol.appendChild(document.createTextNode(userId));
+            ol.appendChild(li);
+
+            var id = arrayOfTodos[i].id;
+            var li = document.createElement('li');
+            ol.appendChild(document.createTextNode(id));
+            ol.appendChild(li);
+
+            var title = arrayOfTodos[i].title;
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(title));
+            ol.appendChild(li);
+
+            var completed = arrayOfTodos[i].userId;
+            var li = document.createElement('li');
+            ol.appendChild(document.createTextNode(completed));
+            ol.appendChild(li);
+        }
     }
