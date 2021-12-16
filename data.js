bd1d41b8f7531025258b6f693834dcbb5962b2a6
RWelchIII <Avidcompassion@gmail.com>
2021-12-16 11:05:50 -0600

    update to match main

--- data.js
+++ data.js
@@ -1,22 +1,4 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
-    console.log(arrayOfTodos[0].userId)
-    console.log(arrayOfTodos[1].userId)
- 
-    const fetchTodos = () => {
+ const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
         .then( (response) => response.json())
         .then( (json) => arrayOfTodos = json)
