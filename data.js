f9d10a62ec6b74e18d62c4a1d223f3141ce109c2
RWelchIII <Avidcompassion@gmail.com>
2021-12-16 11:02:45 -0600

    change to html

--- data.js
+++ data.js
@@ -1,22 +1,4 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
-    console.log(arrayOfTodos[0].userId)
-    console.log(arrayOfTodos[1].userId)
- 
-    const fetchTodos = () => {
+     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
         .then( (response) => response.json())
         .then( (json) => arrayOfTodos = json)
@@ -24,7 +6,7 @@
     }
     
     const logTodos = () => {
-        console.log(arrayOfTodos)
+        
     }
     
     const populateTodos = () => {
@@ -36,6 +18,7 @@
             var li = document.createElement('li');
             ol.appendChild(document.createTextNode(userId));
             ol.appendChild(li);
+
 
             var id = arrayOfTodos[i].id;
             var li = document.createElement('li');
